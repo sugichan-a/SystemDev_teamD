e5ce16abd52652a442d70b41a12cdeb9af958f9c
sugichan-a <sugihara200303@gmail.com>
2025-05-26 12:50:17 +0900

    画面遷移

---
+++ frontend/src/Pages/DeliveryEditPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const [PageName] = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">[ページタイトル]</h1>
+      <p className="text-gray-600">ここに[ページタイトル]の内容を追加します。</p>
+    </div>
+  );
+};
+
+export default [PageName];
