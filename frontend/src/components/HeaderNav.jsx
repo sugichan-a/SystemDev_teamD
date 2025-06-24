e9afe9096262b0b5c2439f4b6a0f44f9b8d61c41
Yagi-cry <s2370623@s.do-johodai.ac.jp>
2025-06-24 13:05:45 +0900

    HeaderNavいじりました

--- frontend/src/components/HeaderNav.jsx
+++ frontend/src/components/HeaderNav.jsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import StoreInfo from './StoreInfo';
 
 const HeaderNav = () => {
-  return (
-    <header
-      style={{
-        width: '100%',
-        height: 56,
-        background: '#1A2550',
-        color: '#fff',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end', // 右寄せ
-        padding: '0 32px',
-        fontSize: 18,
-        fontWeight: 500,
-        letterSpacing: 1,
-      }}
-    >
-      <StoreInfo />
-    </header>
-  );
+  return null; // ヘッダー自体を非表示
 };
 
 export default HeaderNav;
