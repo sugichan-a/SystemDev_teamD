4e79d444a4b289424bcbfd8d19e033c7ebd2f5ca
uo-da <daiki.kimura.0901@gmail.com>
2025-06-24 10:04:40 +0900

    エラー修正

--- frontend/src/components/HeaderNav.jsx
+++ frontend/src/components/HeaderNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '../Button';
+import Button from './Button';
 
 const HeaderNav = () => {
   const navigate = useNavigate();
