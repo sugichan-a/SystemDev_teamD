90885eb6be46cde5edbc7fc76992e9aa01219b18
YUTA1771 <234050@ocsjoho.onmicrosoft.com>
2025-06-26 15:35:29 +0900

    カスタマーページ変更

--- frontend/src/Pages/CustomerPage.jsx
+++ frontend/src/Pages/CustomerPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 import NavButton from '../components/button/NavButton';
 import Breadcrumbs from '../components/breadcrumbs';
 
